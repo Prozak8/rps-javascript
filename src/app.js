a462e3aff4b002036e55da15bf3d427d7c5fd22d
Zak <zakary.howard94@gmail.com>
2018-11-25 16:34:52 +0100

    game logic WIP using globals for element ids, game engine and opponent selection by displaying random icons

---
+++ src/app.js
@@ -0,0 +1,55 @@
+const global = {}
+
+global.rock = document.getElementById('rock');
+global.paper = document.getElementById('paper');
+global.scissors = document.getElementById('scissors');
+global.playerScore = document.getElementById('player-score');
+global.opponentScore = document.getElementById('opponent-score');
+
+const w = "win";
+const l = "lose";
+const d = "draw";
+
+global.resultEngine = [ [1,l,d], [w,1,d], [w,l,1] ]
+
+global.rock.addEventListener('click', clickRock)
+global.paper.addEventListener('click', clickPaper)
+global.scissors.addEventListener('click', clickScissors)
+
+function clickRock() {
+  let opponentChoice = getOpponentChoice();
+  getResult(0, opponentChoice);
+}
+
+function clickPaper() {
+  let opponentChoice = getOpponentChoice();
+  getResult(1, opponentChoice);
+}
+
+function clickScissors() {
+  let opponentChoice = getOpponentChoice();
+  getResult(2, opponentChoice);
+}
+
+function getResult(playerChoice, opponentChoice) {
+  let result = global.resultEngine[opponentChoice][playerChoice];
+  if (result === "win") {
+    playerScore += 1;
+  } else if (result === "lose") {
+    opponentScore += 1;
+  } else {}
+}
+
+function getOpponentChoice() {
+  let choice = Math.floor(Math.random() * 3);
+    global.opponentChoice.innerHtml = ""
+    global.opponentChoice.classList.add('far');
+    if (choice === 0) {      
+      global.opponentChoice.classList.add('fa-hand-rock');
+    } else if (choice === 1) {      
+      global.opponentChoice.classList.add('fa-hand-paper');
+    } else {
+      global.opponentChoice.classList.add('fa-hand-scissors');
+    }
+    return choice;
+}
