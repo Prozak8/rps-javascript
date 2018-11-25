478d5f55fde954544c23a819ebbf167ac54e18b0
Zak <zakary.howard94@gmail.com>
2018-11-25 18:44:14 +0100

    added ids to player choices

--- src/app.js
+++ src/app.js
@@ -5,51 +5,58 @@ global.paper = document.getElementById('paper');
 global.scissors = document.getElementById('scissors');
 global.playerScore = document.getElementById('player-score');
 global.opponentScore = document.getElementById('opponent-score');
+global.opponentChoice = document.getElementById('opponent-hand-choice');
 
 const w = "win";
 const l = "lose";
 const d = "draw";
 
+let playerScore = 0;
+let opponentScore = 0;
+
 global.resultEngine = [ [1,l,d], [w,1,d], [w,l,1] ]
 
-global.rock.addEventListener('click', clickRock)
-global.paper.addEventListener('click', clickPaper)
-global.scissors.addEventListener('click', clickScissors)
+document.addEventListener('DOMContentLoaded', () => {
 
-function clickRock() {
-  let opponentChoice = getOpponentChoice();
-  getResult(0, opponentChoice);
-}
+  global.rock.addEventListener('click', clickRock)
+  global.paper.addEventListener('click', clickPaper)
+  global.scissors.addEventListener('click', clickScissors)
 
-function clickPaper() {
-  let opponentChoice = getOpponentChoice();
-  getResult(1, opponentChoice);
-}
+  function clickRock() {
+    let opponentChoice = getOpponentChoice();
+    getResult(0, opponentChoice);
+  }
 
-function clickScissors() {
-  let opponentChoice = getOpponentChoice();
-  getResult(2, opponentChoice);
-}
+  function clickPaper() {
+    let opponentChoice = getOpponentChoice();
+    getResult(1, opponentChoice);
+  }
 
-function getResult(playerChoice, opponentChoice) {
-  let result = global.resultEngine[opponentChoice][playerChoice];
-  if (result === "win") {
-    playerScore += 1;
-  } else if (result === "lose") {
-    opponentScore += 1;
-  } else {}
-}
+  function clickScissors() {
+    let opponentChoice = getOpponentChoice();
+    getResult(2, opponentChoice);
+  }
 
-function getOpponentChoice() {
-  let choice = Math.floor(Math.random() * 3);
-    global.opponentChoice.innerHtml = ""
-    global.opponentChoice.classList.add('far');
-    if (choice === 0) {      
-      global.opponentChoice.classList.add('fa-hand-rock');
-    } else if (choice === 1) {      
-      global.opponentChoice.classList.add('fa-hand-paper');
-    } else {
-      global.opponentChoice.classList.add('fa-hand-scissors');
-    }
-    return choice;
-}
+  function getResult(playerChoice, opponentChoice) {
+    let result = global.resultEngine[opponentChoice][playerChoice];
+    if (result === "win") {
+      playerScore += 1;
+    } else if (result === "lose") {
+      opponentScore += 1;
+    } else {}
+  }
+
+  function getOpponentChoice() {
+    let choice = Math.floor(Math.random() * 3);
+      global.opponentChoice.innerHtml = ""
+      global.opponentChoice.classList.add('far');
+      if (choice === 0) {      
+        global.opponentChoice.classList.add('fa-hand-rock');
+      } else if (choice === 1) {      
+        global.opponentChoice.classList.add('fa-hand-paper');
+      } else {
+        global.opponentChoice.classList.add('fa-hand-scissors');
+      }
+      return choice;
+  }
+})
